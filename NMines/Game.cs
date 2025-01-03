fb3955fa27ed5fa8afafabaad74837483ea3d6cf
nshib00 <nshib.54321@gmail.com>
2025-01-03 14:07:05 +0300

    Move game UI in GameUI class

--- NMines/Game.cs
+++ NMines/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NMines.Widgets;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
@@ -10,6 +11,7 @@ namespace NMines
         private GameLevel level;
         private GameConfig config;
 
+        private GameUI gameUI;
         private Map map;
         private MapWidget mapWidget;
 
@@ -36,69 +38,7 @@ namespace NMines
         {
             var gameConfigs = GetGameConfigs();
             config = gameConfigs[level];
-        }
-
-        private Panel CreateTopPanel()
-        {
-            Panel topPanel = new Panel()
-            {
-                Dock = DockStyle.Fill,
-                BackColor = Color.LightGray,
-            };
-
-            TableLayoutPanel layout = new TableLayoutPanel()
-            {
-                Dock = DockStyle.Fill,
-                ColumnCount = 3,
-                RowCount = 1,
-            };
-
-            // layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
-            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45));
-            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
-            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45));
-
-            //ComboBox changeLevelBox = new ComboBox()
-            //{
-            //    DataSource = GetGameConfigs().Values.ToList(),
-            //};
-
-            Label timeLabel = new Label()
-            {
-                Text = "00:00",
-                MinimumSize = new Size(40, 30),
-                Dock = DockStyle.Fill,
-                TextAlign = ContentAlignment.MiddleCenter,
-                Font = new Font("Segoe UI", 18)
-            };
-
-            Button restartButton = new Button()
-            {
-                Text = "🙂",
-                MinimumSize = new Size(40, 40),
-                Dock = DockStyle.Fill,
-                Font = new Font("Segoe UI", 18),
-            };
-            restartButton.Click += restartButton_Click;
-
-            Label minesCountLabel = new Label()
-            {
-                Text = config.MinesCount.ToString(),
-                MinimumSize = new Size(40, 30),
-                Dock = DockStyle.Fill,
-                TextAlign = ContentAlignment.MiddleCenter,
-                Font = new Font("Segoe UI", 18)
-            };
-
-
-            //layout.Controls.Add(changeLevelBox, 0, 0);
-            layout.Controls.Add(timeLabel, 0, 0);
-            layout.Controls.Add(restartButton, 1, 0);
-            layout.Controls.Add(minesCountLabel, 2, 0);
-
-            topPanel.Controls.Add(layout);
-
-            return topPanel;
+            gameUI = new GameUI(config);
         }
 
         private void restartButton_Click(object sender, EventArgs e)
@@ -119,26 +59,26 @@ namespace NMines
                 RowCount = 2,
             };
 
-            mainLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, config.TopFieldHeight));
-            mainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
-
-            Panel topPanel = CreateTopPanel();
-            mainLayout.Controls.Add(topPanel, 0, 0);
-
             map = new Map(config.RowsCount, config.ColsCount, config.MinesCount);
             mapWidget = new MapWidget(form, map, config.CellSize, config.XPad, config.YPad, config.TopFieldHeight);
             map.InitField();
-            //map.SeedMines();
-            //map.CountMinesAroundCells();
 
             mapWidget.ConfigureSize();
             mapWidget.InitCells();
 
+            gameUI.RestartButton.Click += restartButton_Click;
+
             form.MinimumSize = new Size(mapWidget.Width + 20, mapWidget.Height + 20);
 
+            mainLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, config.TopFieldHeight));
+            mainLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+
+            mainLayout.Controls.Add(gameUI.TopPanel, 0, 0);
             mainLayout.Controls.Add(mapWidget, 0, 1);
 
             form.Controls.Add(mainLayout);
+
+
         }
     }
 }
---
+++ NMines/Widgets/GameUI.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace NMines.Widgets
+{
+    public class GameUI
+    {
+        public Panel TopPanel { get; private set; }
+        public static Label TimeLabel { get; private set; }
+        public static Label MinesCountLabel { get; private set; }
+        public Button RestartButton { get; private set; }
+
+        public GameUI(GameConfig config)
+        {
+            InitTopPanel(config);
+        }
+
+        private void InitTopPanel(GameConfig config)
+        {
+            TopPanel = new Panel()
+            {
+                Dock = DockStyle.Fill,
+                BackColor = Color.LightGray,
+            };
+
+            TableLayoutPanel layout = new TableLayoutPanel()
+            {
+                Dock = DockStyle.Fill,
+                ColumnCount = 3,
+                RowCount = 1,
+            };
+
+            // layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45));
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45));
+
+            //ComboBox changeLevelBox = new ComboBox()
+            //{
+            //    DataSource = GetGameConfigs().Values.ToList(),
+            //};
+
+            TimeLabel = new Label()
+            {
+                Text = "00:00",
+                MinimumSize = new Size(40, 30),
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Segoe UI", 18)
+            };
+
+            RestartButton = new Button()
+            {
+                Text = "🙂",
+                MinimumSize = new Size(40, 40),
+                Dock = DockStyle.Fill,
+                Font = new Font("Segoe UI", 18),
+            };
+
+            MinesCountLabel = new Label()
+            {
+                Text = config.MinesCount.ToString(),
+                MinimumSize = new Size(40, 30),
+                Dock = DockStyle.Fill,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Segoe UI", 18)
+            };
+
+
+            //layout.Controls.Add(changeLevelBox, 0, 0);
+            layout.Controls.Add(TimeLabel, 0, 0);
+            layout.Controls.Add(RestartButton, 1, 0);
+            layout.Controls.Add(MinesCountLabel, 2, 0);
+
+            TopPanel.Controls.Add(layout);
+        }
+    }
+}
